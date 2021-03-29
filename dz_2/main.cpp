ec2b6312da658ed42a4e30e5f71f0a5604254997
bnp800 <bnp800onsteam@gmail.com>
2021-03-29 16:58:04 +0800

    dz_2 edit#2

--- dz_2/main.cpp
+++ dz_2/main.cpp
@@ -4,16 +4,19 @@
 using namespace std;
 
 typedef std::complex<double> complexd;
-void Qubit(complexd *in, complexd *out, complexd U[2][2], int nqubits, int size, int left, int k)
+
+void Qubit(complexd *op1, complexd *op2, complexd *out, complexd U[2][2], int nqubits, int size, int left, int k)
 {
     int shift = nqubits - k;
-    int pow2q = 1 << (shift);
+    int pow2q = 1 << shift;
+    //cout << i + left << endl;
+    //int i0 = (i + left) & (~pow2q);
+    //int i1 = (i + left) | pow2q;
+
     for (int i = 0; i < size; i++)
     {
-        int i0 = (i + left) & (~pow2q);
-        int i1 = (i + left) | pow2q;
         int iq = ((i + left) & pow2q) >> shift;
-        out[i] = U[iq][0] * in[i0] + U[iq][1] * in[i1];
+        out[i] = U[iq][0] * op1[i] + U[iq][1] * op2[i];
     }
 }
 
@@ -43,26 +46,6 @@ void Normalize(complexd *in, unsigned long size, double sum)
     }
 }
 
-void Write(complexd *in, unsigned long size, string name)
-{
-    ofstream myout;
-    myout.open(name);
-    for (unsigned long i = 0; i < size; i++)
-    {
-        myout << fixed << setprecision(2) << in[i] << " ";
-    }
-    myout.close();
-}
-
-void Read(complexd *in, unsigned long size, string name)
-{
-    ifstream myin(name);
-    for (unsigned long i = 0; i < size; i++)
-    {
-        myin >> in[i];
-    }
-}
-
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
@@ -81,61 +64,148 @@ int main(int argc, char *argv[])
     int myright = (myrank + 1) * partion_size - 1;
     double totalsum;
     int mode = atoi(argv[3]);
+    int *indexleft = new int[world_size], *indexright = new int[world_size];
     if (myright >= size)
     {
         myright = size - 1;
     }
     partion_size = myright - myleft + 1;
-    int *recvcount, *offset;
-    recvcount = new int[world_size];
-    offset = new int[world_size];
-    offset[myrank] = myrank * partion_size;
-    recvcount[myrank] = partion_size;
-
-    MPI_Allgather(offset + myrank, 1, MPI_INT, offset, 1, MPI_INT, MPI_COMM_WORLD);
-    MPI_Allgather(recvcount + myrank, 1, MPI_INT, recvcount, 1, MPI_INT, MPI_COMM_WORLD);
+    indexleft[myrank] = myleft;
+    indexright[myrank] = myright;
+    MPI_Allgather(indexleft + myrank, 1, MPI_INT, indexleft, 1, MPI_INT, MPI_COMM_WORLD);
+    MPI_Allgather(indexright + myrank, 1, MPI_INT, indexright, 1, MPI_INT, MPI_COMM_WORLD);
 
     in = new complexd[partion_size];
     out = new complexd[partion_size];
-    all = new complexd[size];
 
     if (mode == 1)
     {
-        string name = "in.txt";
         srand(myrank * time(NULL));
         Init_vector(in, partion_size);
         double sum = Get_sum(in, partion_size);
         MPI_Reduce(&sum, &totalsum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
         MPI_Bcast(&totalsum, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         Normalize(in, partion_size, totalsum);
-        MPI_Allgatherv(in, partion_size, MPI_DOUBLE_COMPLEX, all, recvcount, offset, MPI_DOUBLE_COMPLEX, MPI_COMM_WORLD);
-        Write(all, size, name);
+
+        MPI_File fout;
+        MPI_File_open(MPI_COMM_WORLD, "vector.txt", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fout);
+        MPI_File_write_ordered(fout, in, partion_size, MPI_DOUBLE_COMPLEX, MPI_STATUS_IGNORE);
+        MPI_File_close(&fout);
     }
 
     if (mode == 2)
     {
-        string name = "in.txt";
-        Read(all, size, name);
+        string name = "res" + to_string(world_size) + ".txt";
+        MPI_File fin, fout;
+        MPI_File_open(MPI_COMM_WORLD, "vector.txt", MPI_MODE_RDONLY, MPI_INFO_NULL, &fin);
+        MPI_File_open(MPI_COMM_WORLD, name.c_str(), MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &fout);
+        MPI_File_read_ordered(fin, in, partion_size, MPI_DOUBLE_COMPLEX, MPI_STATUS_IGNORE);
+        complexd *op1, *op2;
+        op1 = new complexd[partion_size];
+        op2 = new complexd[partion_size];
+        for (int cur_rank = 0; cur_rank < world_size; cur_rank++)
+        {
+            if (myrank == cur_rank)
+            {
+                //cout << "Now process " << cur_rank << " colletcting data" << endl;
+                int shift = qnum - target;
+                int pow2q = 1 << shift;
+                for (int i = 0; i < partion_size; i++)
+                {
+                    int i0 = (i + myleft) & (~pow2q);
+                    int i1 = (i + myleft) | pow2q;
+                    if (i0 >= myleft && i0 <= myright)
+                    {
+                        op1[i] = in[i0];
+                    }
+                    else
+                    {
+                        int rank = world_size - 1;
+                        while (indexleft[rank] > i0 || indexright[rank] < i0)
+                            rank--;
+                        //cout << "Process " << myrank << " : need data index " << i0 << " for op1 from process " << rank << " which now it's " << op1[i] << endl;
+                        MPI_Sendrecv(&i0, 1, MPI_INT, rank, 0, op1 + i, 1, MPI_DOUBLE_COMPLEX, rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                        //cout << "Process " << myrank << " : had data index " << i0 << " for op1 from process " << rank << " which now it's " << op1[i] << endl;
+                    }
+                    if (i1 >= myleft && i1 <= myright)
+                    {
+                        op2[i] = in[i1];
+                    }
+                    else
+                    {
+                        int rank = world_size - 1;
+                        while (indexleft[rank] > i1 || indexright[rank] < i1)
+                            rank--;
+                        //cout << "Process " << myrank << " : need data index " << i1 << " for op2 from process " << rank << " which now it's " << op2[i] << endl;
+                        MPI_Sendrecv(&i1, 1, MPI_INT, rank, 0, op2 + i, 1, MPI_DOUBLE_COMPLEX, rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                        //cout << "Process " << myrank << " : had data index " << i1 << " for op2 from process " << rank << " which now it's " << op2[i] << endl;
+                    }
+                }
+                for (int i = 0; i < world_size; i++)
+                {
+                    if (i != myrank)
+                    {
+                        int id = -1;
+                        MPI_Send(&id, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+                    }
+                }
+                cout << "Continue" << endl;
+                //continue;
+                /*flag = 1;
+                MPI_Bcast(&flag, 1, MPI_INT, myrank, MPI_COMM_WORLD);
+                cur_rank++;
+                MPI_Bcast(&cur_rank, 1, MPI_INT, myrank, MPI_COMM_WORLD);
+                cout << "Process " << myrank << " complete collect data, now transfering to process " << cur_rank << " flag = " << flag << endl;
+                //MPI_Barrier(MPI_COMM_WORLD);*/
+            }
+            else
+            {
+                //cout << "Process " << myrank << " sending data to process " << cur_rank << /*" flag = " << flag << */ endl;
+                int target_id;
+                //MPI_Probe(cur_rank, 0, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
+                while (1)
+                {
+                    //cout << "Process " << myrank << " wait message from process " << cur_rank << endl;
+                    MPI_Recv(&target_id, 1, MPI_INT, cur_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    if (target_id == -1)
+                        break;
+                    MPI_Send(in - myleft + target_id, 1, MPI_DOUBLE_COMPLEX, cur_rank, 0, MPI_COMM_WORLD);
+                    //cout << "Process " << myrank << " : sent data index " << target_id << " to process " << cur_rank << " which now it's " << in[target_id - myleft] << endl;
+                    //MPI_Probe(cur_rank, 0, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
+                }
+                /*flag = 0;
+                MPI_Bcast(&flag, 1, MPI_INT, cur_rank, MPI_COMM_WORLD);
+                //MPI_Barrier(MPI_COMM_WORLD);*/
+            }
+        }
         my_begin = MPI_Wtime();
-        Qubit(all, out, U, qnum, partion_size, myleft, target);
+        MPI_Barrier(MPI_COMM_WORLD);
+        Qubit(op1, op2, out, U, qnum, partion_size, myleft, target);
+        MPI_Barrier(MPI_COMM_WORLD);
         my_end = MPI_Wtime();
         my_time = my_end - my_begin;
         MPI_Reduce(&my_time, &total_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-        MPI_Gatherv(out, partion_size, MPI_DOUBLE_COMPLEX, all, recvcount, offset, MPI_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD);
-
+        total_time /= world_size;
+        MPI_File_write_ordered(fout, out, partion_size, MPI_DOUBLE_COMPLEX, MPI_STATUS_IGNORE);
+        MPI_File_close(&fin);
+        MPI_File_close(&fout);
+        //MPI_Gatherv(out, partion_size, MPI_DOUBLE_COMPLEX, all, recvcount, offset, MPI_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD);
+        //name = "res" + to_string(world_size) + to_string(myrank) + ".txt";
+        //Write(out, size, name);
         if (!myrank)
         {
-            string name = "res" + to_string(world_size) + ".txt";
+
             cout << "Qnum: " << qnum << endl;
             cout << "Pnum: " << world_size << endl;
             cout << "Target: " << target << endl;
             cout << "Time: " << total_time << " s" << endl;
-            Write(all, size, name);
         }
+        delete[] op1;
+        delete[] op2;
     }
-    delete[] recvcount;
-    delete[] offset;
-    delete[] all;
-
+    delete[] in;
+    delete[] out;
+    delete[] indexleft;
+    delete[] indexright;
     MPI_Finalize();
 }
--- dz_2/test.cpp
+++ dz_2/test.cpp
@@ -5,23 +5,26 @@ typedef std::complex<double> complexd;
 
 int main(int argc, char *argv[])
 {
-    ifstream f1("res1.txt"), f2("res2.txt"), f4("res4.txt"), f8("res8.txt");
+    ifstream f1("res1.txt", ios::binary | ios::in), f2("res2.txt", ios::binary | ios::in), f4("res4.txt", ios::binary | ios::in), f8("res8.txt", ios::binary | ios::in);
     int qnum = atoi(argv[1]);
     unsigned long size = 1 << qnum;
     bool flag = true;
     complexd tmp1, tmp2, tmp4, tmp8;
-    for (unsigned long  i = 0; i < size; i++) {
+    for (unsigned long i = 0; i < size; i++)
+    {
         f1 >> tmp1;
         f2 >> tmp2;
         f4 >> tmp4;
         f8 >> tmp8;
-        if (!(tmp1 == tmp2 && tmp4 == tmp8 && tmp1 == tmp8)) {
+        if (!(tmp1 == tmp2 && tmp4 == tmp8 && tmp1 == tmp8))
+        {
             flag = false;
             cout << "Error! " << i << tmp1 << tmp2 << tmp4 << tmp8 << endl;
-            break; 
+            break;
         }
     }
-    if (flag) {
+    if (flag)
+    {
         cout << "Correct!" << endl;
     }
 
